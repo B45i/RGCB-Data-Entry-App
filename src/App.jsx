949e2ddc3abd1345070d4e21f9ec002e185eb73b
B45i <amalshajan2011@gmail.com>
2021-10-17 00:08:13 +0530

    intial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">RGCB Data</span>
+                </div>
+            </nav>
+
+            <div></div>
+        </>
+    );
+}
+
+export default App;
